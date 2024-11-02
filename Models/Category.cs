ddb8622c3463cb51b391430c8294b73072a4bc94
LuisMain07 <bdmainguez00@gmail.com>
2024-11-02 18:28:06 -0500

    Commit new Guia

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
