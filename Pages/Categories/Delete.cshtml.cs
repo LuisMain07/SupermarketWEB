1ff3ef925239c9d8b966c7f6f22761f66b2e9659
LuisMain07 <bdmainguez00@gmail.com>
2024-11-04 17:22:55 -0500

    Delete  Categories

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FindAsync(id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+
+}
