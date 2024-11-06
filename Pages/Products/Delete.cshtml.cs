ce10bd1eea51660f01c06c98c95044ac45e06581
LuisMain07 <bdmainguez00@gmail.com>
2024-11-05 23:43:50 -0500

    Avance Products Delete

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWeb.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Products { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Products = products;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FindAsync(id);
+
+            if (products != null)
+            {
+                Products = products;
+                _context.Products.Remove(Products);
+                await _context.SaveChangesAsync();
+            }
+
+
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
